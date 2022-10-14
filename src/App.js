ba29d3b91bd661c6c8d72242cf9d00c0138379d3
elisabethfarfan <elisabethfarfan@gmail.com>
2022-10-13 23:23:54 -0500

    creando componente character

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import imagenRickyMorty from './img/rick-morty.png';
 import './App.css';
+import { useState } from 'react';
+import { Character } from './components/Character';
 
 function App() {
+  const [characters, setCharacters] = useState(null);
 
-  const reqApi = ()=>{
-    console.log("click");
-  }
+  const reqApi = async()=>{
+    const api = await fetch('https://rickandmortyapi.com/api/character');
+    const characterApi = await api.json();
+    setCharacters(characterApi.results);
+}
 
   return (
     <div className="App">
       <header className="App-header">
        <h1 className='title'>Rick & Morty</h1>
+
+       {characters ? (
+       <Character characters={characters} setCharacters={setCharacters}/>
+       ) : (
+        <>
        <img src={imagenRickyMorty} alt="Rick & Morty" className='img-home'/>
        <button  onClick={reqApi} className="btn-search">Buscar Personajes</button>
+       </>
+       )}
       </header>
     </div>
   );
